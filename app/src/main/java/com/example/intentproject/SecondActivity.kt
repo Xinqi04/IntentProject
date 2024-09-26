c32658d959a6b61b9af8f4b8b7d1e892c0b393aa
Xinqi04 <155415026+Xinqi04@users.noreply.github.com>
2024-09-26 12:06:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intentproject/SecondActivity.kt
@@ -0,0 +1,46 @@
+package com.example.intentproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    val EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE"
+    val EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+
+        val intent = intent
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val textView = findViewById<TextView>(R.id.text_message)
+        textView.text = message
+
+        val mReply = findViewById<EditText>(R.id.editText_second)
+        val btnKirimSecond = findViewById<Button>(R.id.button_second)
+
+
+        btnKirimSecond.setOnClickListener(View.OnClickListener {
+            val reply = mReply?.text.toString()
+            val replyIntent = Intent()
+            replyIntent.putExtra(EXTRA_REPLY, reply)
+            setResult(RESULT_OK, replyIntent)
+            finish()
+        })
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
